4be7b15cc128eb3c3620cdfe4c9857bcdddca21a
Jyrki Rajala <jor@APPrentice.local>
2014-05-07 13:00:30 +0200

    Init

---
+++ DinnerTime/ITHSViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITHSViewController.h
+//  DinnerTime
+//
+//  Created by Jyrki Rajala on 2014-05-07.
+//  Copyright (c) 2014 Jyrki Rajala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITHSViewController : UIViewController
+
+@end
