651bac68637fae82c0ce55035fba74be40191e4b
Jyrki Rajala <jyrki.rajala@gmail.com>
2014-05-19 00:50:34 +0200

    Added a bunch of views & tables. Also json-stuff to retrieve data from matapi.se.

---
+++ DinnerTime/ITHSFoodTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ITHSFoodTableViewController.h
+//  DinnerTime
+//
+//  Created by Jyrki Rajala on 2014-05-18.
+//  Copyright (c) 2014 Jyrki Rajala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ITHSFoodTableViewController : UITableViewController
+
+@end
