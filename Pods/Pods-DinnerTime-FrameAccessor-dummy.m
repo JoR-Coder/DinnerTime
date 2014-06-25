1ee39900bffbe287abb6d8b97568043c54e38461
Jyrki Rajala <jyrki.rajala@gmail.com>
2014-06-25 14:26:02 +0200

    Added the Pod files.

---
+++ Pods/Pods-DinnerTime-FrameAccessor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DinnerTime_FrameAccessor : NSObject
+@end
+@implementation PodsDummy_Pods_DinnerTime_FrameAccessor
+@end
---
+++ Pods/Pods-DinnerTime-GraphKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DinnerTime_GraphKit : NSObject
+@end
+@implementation PodsDummy_Pods_DinnerTime_GraphKit
+@end
